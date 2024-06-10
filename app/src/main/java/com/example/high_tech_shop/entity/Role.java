c5cfad880b2a569f74c289b7477c2ab68c2e6d58
hieu15402 <hieunm15402@gmail.com>
2024-06-11 01:59:22 +0700

    create-role-entity

---
+++ app/src/main/java/com/example/high_tech_shop/entity/Role.java
@@ -0,0 +1,11 @@
+package com.example.high_tech_shop.entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+public enum Role {
+    ADMIN,
+    USER,
+    SHIPPER
+}
+
--- app/src/main/java/com/example/high_tech_shop/entity/User.java
+++ app/src/main/java/com/example/high_tech_shop/entity/User.java
@@ -14,7 +14,90 @@ public class User {
     private boolean status;
     private String password;
     private String fullName;
+    private Role role;
 
+    public User(int id, String email, String phone, String image, String dob, boolean status, String password, String fullName, Role role) {
+        this.id = id;
+        this.email = email;
+        this.phone = phone;
+        this.image = image;
+        this.dob = dob;
+        this.status = status;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
 
