11068cfd830b155c777339bf27b9939c353a6011
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-06-11 02:07:50 +0700

    delete role

--- app/src/main/java/com/example/high_tech_shop/common/Converters.java
+++ app/src/main/java/com/example/high_tech_shop/common/Converters.java
@@ -2,8 +2,6 @@ package com.example.high_tech_shop.common;
 
 import androidx.room.TypeConverter;
 
-import com.example.high_tech_shop.entity.Role;
-
 public class Converters {
     @TypeConverter
     public static String fromRoleToString(Role role) {
