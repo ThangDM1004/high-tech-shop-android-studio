ac3f683033889c655b6c02a7f65560a9461237c4
hieu15402 <hieunm15402@gmail.com>
2024-06-18 18:05:11 +0700

    user-ui

--- app/src/main/java/com/example/high_tech_shop/user/HomePageActivity.java
+++ app/src/main/java/com/example/high_tech_shop/user/HomePageActivity.java
@@ -1,4 +1,4 @@
-package com.example.high_tech_shop;
+package com.example.high_tech_shop.user;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.high_tech_shop.R;
 
 public class HomePageActivity extends AppCompatActivity {
 
